b1f166d847fc39068cbe4ae3823693f3cd8e7179
Måns Regin <Mans.Regin@se.experis.com>
2021-07-20 11:26:47 +0200

    removed '?'

--- Part B/Chinook/Models/Customer.cs
+++ Part B/Chinook/Models/Customer.cs
@@ -12,8 +12,8 @@ namespace Chinook.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Country { get; set; }
-        public string? PostalCode { get; set; }
-        public string? PhoneNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
     }
 }
