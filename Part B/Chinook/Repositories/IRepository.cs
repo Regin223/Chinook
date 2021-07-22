4f78d28d1626a9e14df8930da95dae653ea5fd00
makjohansson <marcuslh84@gmail.com>
2021-07-22 07:56:26 +0200

    fix: removed unnecessary 'using'

--- Part B/Chinook/Repositories/IRepository.cs
+++ Part B/Chinook/Repositories/IRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Chinook.Models;
+﻿using System.Collections.Generic;
 
 namespace Chinook.Repositories
 {
